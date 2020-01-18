681404c448ada69060d1730fa4abc75fe5fd50a6
Adrian Carayol Orenes <adrian.carayol@vozelia.com>
2020-01-18 11:48:06 +0100

    master: Ostia pilotes video inserted

---
+++ ostia-pilotes/src/components/FullVideo.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <video autoplay loop id="ostiaPilotesVideo">
+            <source src="@/assets/videos/5O8d_S9yqgtXWL03.mp4" type="video/mp4">
+        </video>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['videoSrc'],
+}
+</script>
+<style>
+
+#ostiaPilotesVideo {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  min-width: 100%;
+  min-height: 100%;
+}
+
+</style>
